e1bdc91f756f0fdc16b6a16580e214f117e56e40
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-09-01 21:24:09 +0530

    Wrote base UI template code for student dashboard

---
+++ src/User/Student/Dashboard/StudentDashboard.tsx
@@ -0,0 +1,11 @@
+const StudentDashboard = () => {
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center">
+                <div>Student Dashboard</div>
+            </div>
+        </>
+    )
+}
+
+export default StudentDashboard
