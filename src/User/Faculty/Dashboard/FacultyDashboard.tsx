ea91783b6a916b4d21228cb5e33a1a7f820271db
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-09-01 21:35:09 +0530

    wrote base UI template for faculty dashboard

---
+++ src/User/Faculty/Dashboard/FacultyDashboard.tsx
@@ -0,0 +1,11 @@
+const FacultyDashboard = () => {
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center">
+                <div>Faculty Dashboard</div>
+            </div>
+        </>
+    )
+}
+
+export default FacultyDashboard
