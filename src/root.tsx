37bab27a1af737d76a55a1db0567918cd6afbbb1
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-12-19 22:37:57 +0530

    updated App file path

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,7 @@
 import "./global.css";
 import { createRoot } from "react-dom/client";
 import { AppProvider } from "./app/providers/AppProvider.tsx";
-import App from "./app.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <>
